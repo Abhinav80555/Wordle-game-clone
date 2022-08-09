4e370406a1c1348ce8772db32c8fd24eafaec527
Abhinav80555 <abhinavsekar1515@gmail.com>
2022-08-09 11:13:53 +0530

    heroku

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -4,7 +4,7 @@ export default function Keypad({ usedKeys }) {
   const [letters, setLetters] = useState(null)
 
   useEffect(() => {
-    fetch('http://localhost:3001/letters')
+    fetch('https://peaceful-llama-5b1f55.netlify.app/letters')
       .then(res => res.json())
       .then(json => {
         setLetters(json)
